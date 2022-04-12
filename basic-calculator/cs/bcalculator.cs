cab1602b1aa935cbf3488c276b8588dca5efc8b2
Mourud Ishmam Ahmed <ishmam1@gmail.com>
2022-04-12 02:01:20 -0700

    implemented basic calculator in c#

---
+++ basic-calculator/cs/bcalculator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class BCalculator{
+    static void Main(){
+        Console.WriteLine("Enter an integer: ");
+        double num1 = Int32.Parse(Console.ReadLine());
+        Console.WriteLine("Enter another integer: ");
+        double num2 = Int32.Parse(Console.ReadLine());
+
+        Console.WriteLine($"{num1} + {num2} = {num1 + num2}");
+        Console.WriteLine($"{num1} - {num2} = {num1 - num2}");
+        Console.WriteLine($"{num1} * {num2} = {num1 * num2}");
+        Console.WriteLine($"{num1} / {num2} = {num1 / num2}");
+    }
+
+}
+
