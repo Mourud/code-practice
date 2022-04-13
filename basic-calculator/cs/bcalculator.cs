e63ba56f247f3b04da3f33dbd79b03b68204a356
Mourud Ishmam Ahmed <ishmam1@gmail.com>
2022-04-13 00:03:03 -0700

    fixed errors and removed project files in C#

--- basic-calculator/cs/bcalculator.cs
+++ basic-calculator/cs/bcalculator.cs
@@ -3,9 +3,11 @@
 class BCalculator{
     static void Main(){
         Console.WriteLine("Enter an integer: ");
-        double num1 = Int32.Parse(Console.ReadLine());
+        double num1;
+        Double.TryParse(Console.ReadLine(), out num1);
         Console.WriteLine("Enter another integer: ");
-        double num2 = Int32.Parse(Console.ReadLine());
+        double num2;
+        Double.TryParse(Console.ReadLine(), out num2);
 
         Console.WriteLine($"{num1} + {num2} = {num1 + num2}");
         Console.WriteLine($"{num1} - {num2} = {num1 - num2}");
