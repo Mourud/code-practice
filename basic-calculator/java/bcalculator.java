b2f23b0174ae6437a47f08db60d948c1503dd8e3
Mourud Ishmam Ahmed <ishmam1@gmail.com>
2022-04-12 23:40:36 -0700

    implemmented calculator in java

---
+++ basic-calculator/java/bcalculator.java
@@ -0,0 +1,19 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class bcalculator {
+    public static void main(String[] args) {
+        int num1 = 0, num2 = 0;
+        System.out.println("Enter a number: ");
+        try (Scanner scanner = new Scanner(System.in)) {
+            num1 = scanner.nextInt();
+            System.out.println("Enter another number: ");
+            num2 = scanner .nextInt();
+        }
+        System.out.println(num1 + " + " + num2 + " = " + (num1 + num2));
+        System.out.println(num1 + " - " + num2 + " = " + (num1 - num2));
+        System.out.println(num1 + " * " + num2 + " = " + (num1 * num2));
+        System.out.println(num1 + " / " + num2 + " = " + ((double) num1 / num2));
+
+    }
+}
